5b660f644d0c32983df70cdd80590258eac3c1cc
projectlithos <projectlithos@gmail.com>
2026-07-21 12:59:47 +0100

    Update OESDK installer to 0.0.13

--- Installer/Update.bat
+++ Installer/Update.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
 
-set "UpdaterVersion=0.0.15.2"
+set "UpdaterVersion=0.0.15.3"
 set "InstallRoot=C:\OESDK"
 set "Repository=ProjectLithos/Lithos"
 set "Branch=main"
